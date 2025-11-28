7abf73e5b2b3e474d8abe2306c264df0dba4ef65
Omkar <Omkar@Omkar>
2025-11-29 00:54:17 +0530

    Pom Done!

--- src/main/java/com/ammroth/framework/config/ConfigReader.java
+++ src/main/java/com/ammroth/framework/config/ConfigReader.java
@@ -1,5 +1,31 @@
 package com.ammroth.framework.config;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
-public class ConfigReader {
+	public class ConfigReader {
+	    private static Properties prop = new Properties();
 
-}
+	    static {
+	        FileInputStream fis = null;
+			try {
+				fis = new FileInputStream("src/main/resources/config.properties");
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	            try {
+					prop.load(fis);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	       
+	    }
+
+	    public static String get(String key) {
+	        return prop.getProperty(key);
+	    }
+	}
+
